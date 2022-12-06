29d3af3350c1aef8c8c40ca6dd74a5b4bfaad839
betir <betirov95@gmail.com>
2022-12-06 15:57:20 +0300

    navigation example

---
+++ app/src/main/java/com/bam/navigationexample/FirstFragment.kt
@@ -0,0 +1,32 @@
+package com.bam.navigationexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.bam.navigationexample.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstBinding.inflate(inflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            Navigation.findNavController(binding.root).navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+
+}
